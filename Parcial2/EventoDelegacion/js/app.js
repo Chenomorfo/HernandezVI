b803fc0d279285ae45ad0657493efc46c20ba290
chenomorfo <angelhdz1502@gmail.com>
2022-10-13 18:47:12 -0500

    updates

--- Parcial2/EventoDelegacion/js/app.js
+++ Parcial2/EventoDelegacion/js/app.js
@@ -2,7 +2,8 @@ const items = document.querySelectorAll('li')
 
 items.forEach((item)=>{
     item.addEventListener('click',()=>{
-        /* alert('Esta etiqueta dice: '+item.innerHTML) */
-       item.classList.toggle('delegacion')
+       item.classList.toggle('delegado')
     })
 })
+
+
