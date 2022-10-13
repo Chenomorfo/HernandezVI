49caf4cd2035ddb99f235127839ce3e6e1fb2e73
chenomorfo <angel_humberto_1502@hotmail.com>
2022-10-13 16:58:05 -0500

    actualizando

---
+++ Parcial2/EjemploJQuery/js/app.js
@@ -0,0 +1,17 @@
+var msj
+$(document).ready(function(){
+    console.log('Estoy vivo')
+
+    $('#evento').click(function(){
+        for(i=0;i<1000;i++){
+
+        }
+    })
+
+
+
+})
+
+myBug() = function(){
+    
+}
--- Parcial2/RegistroDelegacion/js/app.js
+++ Parcial2/RegistroDelegacion/js/app.js
@@ -3,6 +3,6 @@ const items = document.querySelectorAll('li')
 items.forEach((item)=>{
     item.addEventListener('click',()=>{
         /* alert('Esta etiqueta dice: '+item.innerHTML) */
-        document.getElementById('evento').innerHTML = 'Esta etiqueta dice: '+item.innerHTML
+       item.classList.toggle('delegacion')
     })
 })
